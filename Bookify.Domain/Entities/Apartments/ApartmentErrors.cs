6d90b1051538ab4a66457402b86290b88aed4dbb
BrunoSouza94 <bruno.souza_@outlook.com.br>
2024-04-11 18:39:13 -0300

    Finishing the domain layer, adding errors and repository to all entities.

---
+++ Bookify.Domain/Entities/Apartments/ApartmentErrors.cs
@@ -0,0 +1,11 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Apartments
+{
+    public static class ApartmentErrors
+    {
+        public static Error NotFound = new(
+            "Apartment.NotFound",
+            "The specified apartment was not found");
+    }
+}
